a8560673d9f99f49c8df9532dd23f1e13ed6959b
AMCocan <AMCocan@student.fullsail.edu>
2023-02-08 16:40:18 -0500

    Initiate functions in snake_utills.py

--- Utills/snake_utills.py
+++ Utills/snake_utills.py
@@ -9,5 +9,41 @@ import random as rn
 import time as tm
 import sys as sy
 
-# ---
+# --- main_process ---
 
+def main_process(size, title, icon):
+
+    # Initialize Pygame.
+    pg.init()
+
+    # Read agrs and build window.
+    pg.display.set_mode(size)
+    pg.display.set_caption(title)
+    pg.display.set_icon(icon)
+
+    # Create window and run main process here.
+    while running:
+
+        for event in pg.event.get():
+
+            if event.type == pg.QUIT:
+
+                running = False
+    
+        pg.display.flip()
+
+# --- frame_rate ---
+
+def frame_rate(x):
+    
+    # Set a static frame rate here.
+    pass
+
+# --- player ---
+
+def player():
+
+    # Create player here.
+    pass
+
+# ---
