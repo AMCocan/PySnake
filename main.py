bfbe50ba7fb28c0cf392dfc6e9e0e78b2b5fbff7
AMCocan <AMCocan@student.fullsail.edu>
2023-02-09 04:11:01 -0500

    Add 'ui_colors' to main.py

--- main.py
+++ main.py
@@ -12,8 +12,16 @@ size = [700, 700]
 title = f'PySnake'
 icon = utills.pg.image.load('Assets/icon.png')
 
+ui_colors = {
+    'obj':(0, 0, 0), 
+    'background':(10, 10, 10), 
+    'enemy':(0, 0, 0), 
+    'player':(0, 255, 140), 
+    'key':(250, 250, 250)
+}
+
 # ---
 
-utills.main_process(size, title, icon)
+utills.main_process(size, title, icon, ui_colors)
 
 # ---
