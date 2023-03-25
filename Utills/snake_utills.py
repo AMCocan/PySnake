e862a715cfeb58301e9e052596106e20c8ba146c
AMCocan <AMCocan@student.fullsail.edu>
2023-03-24 22:04:47 -0400

    Add player movement & Clean up project.

--- Utills/snake_utills.py
+++ Utills/snake_utills.py
@@ -21,11 +21,16 @@ def main_process(size, title, icon, ui_colors):
     pg.display.set_caption(title)
     pg.display.set_icon(icon)
 
-    # Set background color fill here.
-    pg.Surface.fill(bgw, ui_colors['background'])
+    # Set the player initial position here.
+    player_xpos_init = size[0]/2
+    player_ypos_init = size[1]/2
 
-    # Set the framerate here.
-    frame_rate(60)
+    # Set player move index here.
+    pos_move_index = 2
+    neg_move_index = -2
+
+    # Create list variable for player position.
+    player_pos = [player_xpos_init, player_ypos_init]
 
     # Main process loop condition
     running = True
@@ -33,32 +38,61 @@ def main_process(size, title, icon, ui_colors):
     # Create window and run main process here.
     while running:
 
+        # Set the framerate here.
+        frame_rate(60)
+
+        # Set background color fill here.
+        pg.Surface.fill(bgw, ui_colors['background'])
+
+        # Key's event listener.
+        keypress = pg.key.get_pressed()
+
+        # Event listener for 'QUIT'
         for event in pg.event.get():
-
             if event.type == pg.QUIT:
-
                 running = False
 
-        player(bgw, ui_colors['player'])
+        # Player movement called here.
+        player_movement(keypress, running, pos_move_index, neg_move_index, player_pos)
 
-        pg.display.update()
+        # Player render called here.
+        player(bgw, ui_colors['player'], player_pos)
+        
+        # Update the screen.
+        pg.display.flip()
     
     # Uninitialize everything and quit.
     pg.quit()
     quit()
 
 # --- frame_rate ---
-
 def frame_rate(x):
-    
+
     # Set a static frame rate here.
     pg.time.Clock().tick(x)
 
-# --- player ---
+# --- player_movement ---
+def player_movement(keypress, running, pos_move_index, neg_move_index, player_pos):
 
-def player(bgw, ui):
+    # Listen and change player position.
+    if keypress[pg.K_ESCAPE]: # Work on this -- Now working rn. -- [!]
+        running = False
+    elif keypress[pg.K_w]:
+        player_pos[1] += neg_move_index
+    elif keypress[pg.K_a]:
+        player_pos[0] += neg_move_index
+    elif keypress[pg.K_s]:
+        player_pos[1] += pos_move_index
+    elif keypress[pg.K_d]:
+        player_pos[0] += pos_move_index
+    
+    # Return list variable for player position.
+    return(player_pos, running)
+
+# --- player ---
+def player(bgw, ui, player_pos):
 
     # Draw player here.
-    pg.draw.rect(bgw, ui, [345, 345, 10, 10])
+    pg.draw.rect(bgw, ui, [player_pos[0], player_pos[1], 10, 10])
 
 # ---
--- main.py
+++ main.py
@@ -13,14 +13,14 @@ title = f'PySnake'
 icon = utills.pg.image.load('Assets/icon.png')
 
 ui_colors = {
-    'obj':(0, 0, 0), 
     'background':(10, 10, 10), 
-    'enemy':(0, 0, 0), 
     'player':(0, 255, 140), 
+    'enemy':(0, 0, 0), 
+    'obj':(0, 0, 0), 
     'key':(250, 250, 250)
 }
 
-# ---
+# --- main_process Loop ---
 
 utills.main_process(size, title, icon, ui_colors)
 
